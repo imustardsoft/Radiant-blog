74545e288575198a99ca0ace9f21ebfa717d1b48
star <jn615@126.com>
2010-09-18 13:57:19 +0800

    test

--- radiant-blog/db/migrate/20100916033246_add_column_blog_directory_to_pages.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnBlogDirectoryToPages < ActiveRecord::Migration
-  def self.up
-    add_column :pages, :blog_directory, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :comments, :blog_directory
-  end
-end
