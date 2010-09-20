6faf7db5b5c21d0654bd0f87bcae66ed89862d7e
star <jn615@126.com>
2010-09-20 11:41:17 +0800

    s

--- radiant-blog/db/migrate/20100916033142_add_column_bloger_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnBlogerToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :bloger, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :comments, :bloger
-  end
-end
